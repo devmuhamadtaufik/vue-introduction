347e44ade368810d5e98157289c282320a75205a
devmuhamadtaufik <devmuhamadtaufik@gmail.com>
2023-05-29 20:25:27 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Frame from "./components/Frame.vue";
+
+function redirectToMain() {
+  window.location.href = "http://www.w3schools.com";
+}
+</script>
+
+<template>
+  <Frame
+    class="text-black"
+    title="ikan mas"
+    message="Ikan mas adalah ikan yang berwarna emas"
+    @masuk="redirectToMain"
+  >
+    <template #image>
+      <img
+        src="https://blue.kumparan.com/image/upload/v1634025439/01gkk38w81p6gf02yzv7zzkep7.jpg"
+        class="h-full object-cover object-right"
+      />
+    </template>
+  </Frame>
+</template>
---
+++ src/components/Frame.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-white flex flex-col items-center">
+    <div>Ini gambar {{ title }}</div>
+    <div
+      class="bg-sky-500 rounded w-60 h-80 flex items-center justify-center my-2"
+      @click="emits('redirect')"
+    >
+      <div class="bg-white w-52 h-72">
+        <slot name="image"></slot>
+      </div>
+    </div>
+    <div>{{ message }}</div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+const emits = defineEmits(["redirect"]);
+const props = defineProps(["title", "message"]);
+</script>
